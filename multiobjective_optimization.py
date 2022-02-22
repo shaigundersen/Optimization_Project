ddd89b57f204a2917a05ef62dacf4198041536b1
SHAI GUNDERSEN <shai.gundersen@gmail.com>
2022-02-22 15:26:35 +0200

    added function docs & small changes making it more modular code

--- multiobjective_optimization.py
+++ multiobjective_optimization.py
@@ -4,21 +4,29 @@ import matplotlib.pyplot as plt
 from math import pi
 from pyomo.environ import *
 
-def epsilone_method(model, f1_expr, f2_expr, solver, f_min, f_max):
-    ########## epsilon ##########
-    # add objective min f1
-    # add constrain f2<=epsilon
-    model.new_obj = Objective(expr=f1_expr, sense=minimize)
+def epsilon_method(model, solver, f2_min, f2_max):
+    """
+    Function for finding Pareto frontier using the epsilon constraint method
+
+    :param model: predefined model initialized with two basic vars (x,y), their boundaries, basic constraints
+                  and two objectives (f1, f2)
+    :param solver: optimization solver used to solve this model
+    :param f2_min: anchor value of f2
+    :param f2_max: anchor value of f2
+    :return: tuple of x,y and f1,f2 values for Pareto frontier
+    """
+    # create new objective  -> min f1
+    # create new constraint -> f2 <= epsilon
+    model.new_obj = Objective(expr=model.f1, sense=minimize)
     model.e = Param(initialize=0, mutable=True)
-    model.c_temp = Constraint(expr=f2_expr <= model.e)
+    model.c_temp = Constraint(expr=model.f2 <= model.e)
 
     # create range of 10 epsilons
     n = 10
-    step = (f_max - f_min) / n
-    epsilons = np.arange(f_min, f_max, step)
+    step = (f2_max - f2_min) / n
+    epsilons = np.arange(f2_min, f2_max, step)
     x_list, y_list = [], []
     f1_list, f2_list = [], []
-
     for ep in epsilons:
         model.e = ep
         solver.solve(model)
@@ -27,12 +35,11 @@ def epsilone_method(model, f1_expr, f2_expr, solver, f_min, f_max):
         f1_list.append(value(model.new_obj))
         f2_list.append(value(model.c_temp))
 
-    # plot(x_list, y_list, f1_list, f2_list, "epsilon_method")
     # delete all temp components
     model.del_component(model.c_temp)
     model.del_component(model.e)
     model.del_component(model.new_obj)
-    return x_list, y_list, f1_list, f2_list, "epsilon_method"
+    return x_list, y_list, f1_list, f2_list, "Epsilon Constraint Method"
 
 def plot(**params):
     X_1, Y_1, F1_1, F2_1, title_1 = params['subplot1']
@@ -75,20 +82,22 @@ def plot(**params):
 
     plt.show()
 
-def weighted_sum(model, f1_expr, f2_expr, solver):
-    ######## weight_sum ##########
-    # create vars for f1 and f2 to retrieve values afterwards
-    model.f1 = Var()
-    model.f2 = Var()
-    model.c_f1 = Constraint(expr=model.f1 == f1_expr)
-    model.c_f2 = Constraint(expr=model.f2 == f2_expr)
+def weighted_sum(model, solver):
+    """
+    Function for finding Pareto frontier using the weighted sum method
 
-    # add objective min alfa*f1 + (1-alfa)*f2
-    x_list, y_list = [], []
-    f1_list, f2_list = [], []
+    :param model: predefined model initialized with two basic vars (x,y), their boundaries, basic constraints
+                  and two objectives (f1, f2)
+    :param solver: optimization solver used to solve this model
+    :return: tuple of x,y and f1,f2 values for Pareto frontier
+    """
+    # create temp components needed for this function
     model.alfa = Param(initialize=0, mutable=True)
+    # create new weighted objective -> min alfa*f1 + (1-alfa)*f2
     model.f = Objective(expr=model.alfa * model.f1 + (1 - model.alfa) * model.f2, sense=minimize)
     alfas = np.arange(0, 1, 0.1)
+    x_list, y_list = [], []
+    f1_list, f2_list = [], []
     for alfa in alfas:
         model.alfa = alfa
         solver.solve(model)
@@ -97,48 +106,45 @@ def weighted_sum(model, f1_expr, f2_expr, solver):
         f1_list.append(value(model.f1))
         f2_list.append(value(model.f2))
 
-    # plot(x_list, y_list, f1_list, f2_list, title="Weight Sum")
     # delete all temp components
     model.del_component(model.f)
     model.del_component(model.alfa)
-    model.del_component(model.f1)
-    model.del_component(model.f2)
-    model.del_component(model.c_f1)
-    model.del_component(model.c_f2)
 
-    return x_list, y_list, f1_list, f2_list, "Weight Sum"
+    return x_list, y_list, f1_list, f2_list, "Weighted Sum Method"
 
 
-def get_pareto_min_max_values(model, f1_expr, f2_expr, solver):
-    model.f1 = Var()
-    model.f2 = Var()
-    model.C_f1 = Constraint(expr=model.f1 == f1_expr)
-    model.C_f2 = Constraint(expr=model.f2 == f2_expr)
+def get_pareto_min_max_values(model, solver):
+    """
+    Util function to find 'Anchor' points. Anchor points describe the extremes of the Pareto frontier.
+    Those points can be obtained when we minimize one objective while ignoring others.
+
+    :param model: predefined model initialized with two basic vars (x,y), their boundaries, basic constraints
+                  and two objectives (f1, f2)
+    :param solver: optimization solver used to solve this model
+    :return: Anchor values from f2 of Pareto frontier
+    """
+    # create temp components needed for this function
     model.O_f1 = Objective(expr=model.f1, sense=minimize)
     model.O_f2 = Objective(expr=model.f2, sense=minimize)
 
-    # max f1 separately
+    # min f1 separately
     model.O_f1.activate()
     model.O_f2.deactivate()
     solver.solve(model)
-
     f2_min = value(model.f2)
 
-    # max f2 separately
+    # min f2 separately
     model.O_f2.activate()
     model.O_f1.deactivate()
     solver = SolverFactory(solvername, executable=solverpath_exe)
     solver.solve(model)
     f2_max = value(model.f2)
+
     # delete all temp components
-    model.del_component(model.C_f1)
-    model.del_component(model.C_f2)
     model.del_component(model.O_f1)
     model.del_component(model.O_f2)
-    model.del_component(model.f2)
-    model.del_component(model.f1)
-    # get range of patron min_max values
-    f2_max, f2_min = round(f2_max), round(f2_min)
+
+    # get Anchor values from f2 of Pareto frontier
     f2_min, f2_max = min(f2_min, f2_max), max(f2_min, f2_max)
     return f2_min, f2_max
 
@@ -152,6 +158,8 @@ if __name__ == '__main__':
     solverpath_exe = sys.argv[2]
     try:
         solver = SolverFactory(solvername, executable=solverpath_exe)
+
+        # Define our model according to the Cones problem #
         model = ConcreteModel()
         # create 2 vars for the problem
         model.x = Var(domain=NonNegativeReals, bounds=(0, 10))
@@ -160,12 +168,15 @@ if __name__ == '__main__':
         model.s = Expression(expr=sqrt((model.x**2) + (model.y**2)))  # s = sqrt(r^2 + h^2)
         # create all constrains on vars
         model.c = Constraint(expr=(pi/3)*(model.x**2)*model.y >= 200)
-        # define objective function expression
-        f1 = pi * model.x * (model.x + model.s)
-        f2 = pi * model.x * model.s
-        f_min, f_max = get_pareto_min_max_values(model, f1, f2, solver)
-        eps_param = epsilone_method(model, f1, f2, solver, f_min, f_max)
-        weighted_param = weighted_sum(model, f1, f2, solver)
+        # define objective function as expressions for later use and manipulating
+        model.f1 = Expression(expr=pi * model.x * (model.x + model.s))
+        model.f2 = Expression(expr=pi * model.x * model.s)
+
+        # Solve model using two methods
+        f_min, f_max = get_pareto_min_max_values(model, solver)
+        eps_param = epsilon_method(model, solver, f_min, f_max)
+        weighted_param = weighted_sum(model, solver)
+        # plot our results
         plot(subplot1=eps_param, subplot2=weighted_param)
-    except:
-        quit("solver doesn't exist")
+    except Exception as e:
+        quit(f"An Error accrued\n{e}")
